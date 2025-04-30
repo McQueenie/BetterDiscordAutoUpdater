079e0bb0dfa12b94e3919c8c7dc4a9a79a500b05
Mateusz <128993103+McQueenie@users.noreply.github.com>
2025-04-30 15:32:50 +0200

    added sh script

---
+++ BetterDiscordAutoUpdater.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Updating BetterDiscord"
+
+# Kill Discord if running
+pkill -f Discord
+
+# Variables setup
+REPO_URL="https://github.com/BetterDiscord/BetterDiscord.git"
+TEMP_DIR="/tmp/BDInstaller"
+
+# Create temporary directory
+mkdir -p $TEMP_DIR
+cd $TEMP_DIR
+
+echo "Cloning BetterDiscord repository from $REPO_URL"
+git clone $REPO_URL
+cd BetterDiscord
+
+echo "Installing BetterDiscord"
+pnpm install
+pnpm build
+pnpm inject
+
+echo "Completed Successfully"
+# Cleanup temporary directory
+# cd $HOME
+# echo "Cleaning up temporary files..."
+# rm -rf $TEMP_DIR
+# echo "Cleanup completed"
+
+# Restart Discord
+open -a Discord
+
+sleep 5
