e08fffa8afa0341876c63e9198111ffb2ea870b6
Mateusz <128993103+McQueenie@users.noreply.github.com>
2025-05-11 21:01:12 +0200

    Fix .bat after last update

--- BetterDiscord-Updater.bat
+++ BetterDiscord-Updater.bat
@@ -6,19 +6,15 @@ taskkill /IM discord.exe /F
 
 ::Variables setup
 set REPO_URL=https://github.com/BetterDiscord/BetterDiscord.git
-set TEMP_DIR=%TEMP%\BDInstaller
-::Create temporary directory
-mkdir %TEMP_DIR%
-cd /d %TEMP_DIR%
 echo Cloning BetterDiscord repository from %REPO_URL%
-git clone %REPO_URL%
+git clone --single-branch -b main %REPO_URL%
 cd BetterDiscord
 echo Installing BetterDiscord
 call pnpm install
 call pnpm build
 call pnpm inject
 echo Completed Successfully
-@REM ::Cleanup temporary directory
+@REM Cleanup temporary directory
 @REM cd /d %USERPROFILE%
 @REM echo Cleaning up temporary files...
 @REM rmdir /s /q %TEMP_DIR%
